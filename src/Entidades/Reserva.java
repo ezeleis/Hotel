73bcd391cf8305882627f645f48875e202a13c12
ezeleis <67750812+ezeleis@users.noreply.github.com>
2023-06-12 16:23:23 -0300

    feature(main): Add try catch block with exceptions

--- src/Entidades/Reserva.java
+++ src/Entidades/Reserva.java
@@ -3,7 +3,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import Excecoes.*;
-class Reserva {
+public class Reserva {
     private int numeroQuarto;
     private LocalDate dataEntrada;
     private LocalDate dataSaida;
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+import Entidades.*;
+import Excecoes.*;
 public class Main {
     public static void main(String[] args) {
 
@@ -16,6 +18,32 @@ public class Main {
         System.out.print("Data de saída (dd/MM/yyyy): ");
         String dataSaidaStr = scanner.next();
         LocalDate dataSaida = LocalDate.parse(dataSaidaStr, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        try {
+            Reserva reserva = new Reserva(numeroQuarto, dataEntrada, dataSaida);
 
+            System.out.println("\nReserva: Quarto " + reserva.getNumeroQuarto() +
+                    ", Entrada: " + reserva.getDataEntrada().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) +
+                    ", Saída: " + reserva.getDataSaida().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) +
+                    ", " + reserva.getDuracaoEmDias() + " noites");
+
+            System.out.println("\nEntre com as datas para atualização da reserva");
+
+            System.out.print("Nova Data de Entrada (dd/MM/yyyy): ");
+            String novaDataEntradaStr = scanner.next();
+            LocalDate novaDataEntrada = LocalDate.parse(novaDataEntradaStr, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+            System.out.print("Nova Data de saída (dd/MM/yyyy): ");
+            String novaDataSaidaStr = scanner.next();
+            LocalDate novaDataSaida = LocalDate.parse(novaDataSaidaStr, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+            reserva.atualizarReserva(novaDataEntrada, novaDataSaida);
+
+            System.out.println("\nReserva atualizada: Quarto " + reserva.getNumeroQuarto() +
+                    ", Entrada: " + reserva.getDataEntrada().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) +
+                    ", Saída: " + reserva.getDataSaida().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) +
+                    ", " + reserva.getDuracaoEmDias() + " noites");
+        } catch (ExcecaoDominio e) {
+            System.out.println("\nErro na Reserva: " + e.getMessage());
+        }
     }
 }
